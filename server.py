cc9a12d7b97e55bfa8a0d40e4c31242edccfe6c5
Jianfei Wang <me@thinxer.com>
2011-10-25 00:41:38 +0800

    refactor code to support connectors

--- server.py
+++ server.py
@@ -13,15 +13,23 @@ import tornado.netutil
 from urllib.parse import urlparse, urlunparse
 from collections import OrderedDict
 
-socks_server = None
-#socks_server = 'localhost'
-#socks_port = 2090
+logging.getLogger().setLevel(logging.INFO)
+
 
 def header_parser(headers):
     for header in headers.split(b'\r\n'):
         i = header.find(b':')
         if i >= 0:
             yield header[:i], header[i+2:]
+
+
+def hostport_parser(hostport, default_port):
+    i = hostport.find(b':')
+    if i >= 0:
+        return hostport[:i], int(hostport[i+1:])
+    else:
+        return hostport, default_port
+
 
 def write_to(stream):
     def on_data(data):
@@ -32,95 +40,118 @@ def write_to(stream):
                 stream.write(data)
     return on_data
 
+
 def pipe(stream_a, stream_b):
     writer_a = write_to(stream_a)
     writer_b = write_to(stream_b)
     stream_a.read_until_close(writer_b, writer_b)
     stream_b.read_until_close(writer_a, writer_a)
 
-# TODO close the incoming connection if the connection failed.
-def socks_connect(stream, host, port, on_connected):
-    def socks_response(data):
-        if data[1] == 0x5a:
-            if on_connected: on_connected()
-        else:
-            raise Exception('socks failed')
 
-    def socks_connected():
-        stream.write(b'\x04\x01' + struct.pack('>H', port) + b'\x00\x00\x00\x09userid\x00' + host + b'\x00')
-        stream.read_bytes(8, socks_response)
+class Connector:
+    def connect(self, host, port, callback):
+        raise NotImplementedError()
 
-    stream.connect((socks_server, socks_port), socks_connected)
 
-def tcp_client(host, port, on_connected = None):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    stream = tornado.iostream.IOStream(s)
-    if socks_server:
-        socks_connect(stream, host, port, on_connected)
-    else:
-        stream.connect((host, port), on_connected)
-    return stream
+class DirectConnector(Connector):
+
+    def connect(self, host, port, callback):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        stream = tornado.iostream.IOStream(s)
+        stream.connect((host, port), lambda: callback(stream))
+
+
+class SocksConnector(Connector):
+
+    def __init__(self, socks_server, socks_port):
+        Connector.__init__(self)
+        self.socks_server = socks_server
+        self.socks_port = socks_port
+
+    def connect(self, host, port, callback):
+
+        def socks_response(data):
+            if data[1] == 0x5a:
+                callback(stream)
+            else:
+                callback(None)
+
+        def socks_connected():
+            stream.write(b'\x04\x01' + struct.pack('>H', port) + b'\x00\x00\x00\x09userid\x00' + host + b'\x00')
+            stream.read_bytes(8, socks_response)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        stream = tornado.iostream.IOStream(s)
+        stream.connect((self.socks_server, self.socks_port), socks_connected)
+
 
 class ProxyHandler:
-    def __init__(self, stream):
+    def __init__(self, stream, connector):
+        self.connector = connector
+
         self.incoming = stream
         self.incoming.read_until(b'\r\n', self.on_method)
+
         self.method = None
-        self.outgoing = None
+        self.url = None
+        self.ver = None
+        self.headers = None
 
     def on_method(self, method):
-        self.method = method
+        self.method, self.url, self.ver = method.strip().split(b' ')
         self.incoming.read_until(b'\r\n\r\n', self.on_headers)
 
-    def on_headers(self, headers):
-        headers = OrderedDict(header_parser(headers))
-        logging.info(self.method.decode('utf8'))
-        method, url, ver = self.method.split(b' ')
-        if method == b'CONNECT':
-            host = url
-            i = host.find(b':')
-            if i >= 0:
-                host, port = host[:i], int(host[i+1:])
-            else:
-                port = 443
-
-            def on_connected():
-                self.incoming.write(b'HTTP/1.0 200 Connection Established\r\n')
-                self.incoming.write(b'\r\n')
-                pipe(self.incoming, self.outgoing)
-
-            self.outgoing = tcp_client(host, port, on_connected)
+    def on_connected(self, outgoing):
+        if outgoing:
+            path = urlunparse((b'', b'') + urlparse(self.url)[2:])
+            outgoing.write(b' '.join((self.method, path, self.ver)) + b'\r\n')
+            for k, v in self.headers.items():
+                outgoing.write(k + b': ' + v + b'\r\n')
+            outgoing.write(b'\r\n')
+            writer_in = write_to(self.incoming)
+            if b'Content-Length' in self.headers:
+                self.incoming.read_bytes(int(self.headers[b'Content-Length']), outgoing.write, outgoing.write)
+            outgoing.read_until_close(writer_in, writer_in)
         else:
-            if b'Proxy-Connection' in headers:
-                del headers[b'Proxy-Connection']
-            headers[b'Connection'] = b'close'
-            if b'Host' in headers:
-                host = headers[b'Host']
-                i = host.find(b':')
-                if i >= 0:
-                    host, port = host[:i], int(host[i+1:])
-                else:
-                    port = 80
+            self.incoming.close()
 
-                def on_connected():
-                    path = urlunparse((b'', b'') + urlparse(url)[2:])
-                    write_to(self.outgoing)(b' '.join((method, path, ver)))
-                    write_to(self.outgoing)(b'\r\n'.join(k + b': ' + v for k, v in headers.items()))
-                    write_to(self.outgoing)(b'\r\n\r\n')
-                    writer_in = write_to(self.incoming)
-                    if b'Content-Length' in headers:
-                        self.incoming.read_bytes(int(headers[b'Content-Length']), self.outgoing.write, self.outgoing.write)
-                    self.outgoing.read_until_close(writer_in, writer_in)
+    def on_connect_connected(self, outgoing):
+        if outgoing:
+            self.incoming.write(b'HTTP/1.0 200 Connection Established\r\n\r\n')
+            pipe(self.incoming, outgoing)
+        else:
+            self.incoming.close()
 
-                self.outgoing = tcp_client(host, port, on_connected)
+    def on_headers(self, headers_buffer):
+        self.headers = OrderedDict(header_parser(headers_buffer))
+        logging.info('%s %s %s', self.method, self.url, self.ver)
+        if self.method == b'CONNECT':
+            host, port = hostport_parser(self.url, 443)
+            self.outgoing = self.connector.connect(host, port, self.on_connect_connected)
+        else:
+            if b'Proxy-Connection' in self.headers:
+                del self.headers[b'Proxy-Connection']
+            self.headers[b'Connection'] = b'close'
+            if b'Host' in self.headers:
+                host, port = hostport_parser(self.headers[b'Host'], 80)
+                self.outgoing = self.connector.connect(host, port, self.on_connected)
             else:
                 self.incoming.close()
 
+
 class ProxyServer(tornado.netutil.TCPServer):
+
+    def __init__(self, connector = None):
+        tornado.netutil.TCPServer.__init__(self)
+        self.connector = connector or DirectConnector()
+
     def handle_stream(self, stream, address):
-        ProxyHandler(stream)
+        ProxyHandler(stream, self.connector)
 
-server = ProxyServer()
-server.listen(8000)
 
-tornado.ioloop.IOLoop.instance().start()
+def main():
+    server = ProxyServer(DirectConnector())
+    server.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__': main()
